cd0a6ccae1aa13fd4fa4d824a97bdd50e5ca0820
conorgee <conorgilmartin13@gmail.com>
2023-04-05 00:26:08 +0100

    fixing bugs in the abstract and sticky note requests

--- api.py
+++ api.py
@@ -1,115 +1,180 @@
+# Import required libraries
+import textwrap
 from flask import Flask, request, jsonify
 from flask_cors import CORS
 from summarizer import Summarizer
-from transformers import pipeline
+from transformers import pipeline, AutoTokenizer
 from textblob import TextBlob
 
 # Load the summarization pipeline
-
 app = Flask(__name__)
 CORS(app)
+abstractSum = pipeline("summarization", model="t5-base")
 
 # Initialize an empty array to store summaries
 summaries = []
-counter = 1
 noteOpen = False
-abstractSum = pipeline("summarization", model="t5-base", tokenizer="t5-base", framework="tf")
 summarizer = Summarizer()
 
-
-@app.route('/summarize', methods=['POST'])
+# Define endpoint to summarize text
+@app.route("/summarize", methods=["POST"])
 def summarize():
     global summarizer
+    # Get input text and number of sentences to summarize
     data = request.get_json()
-    text = data['text']
-    num_sentencess = int(request.args.get('num_sentences', 3))  
+    text = data["text"]
+    # Default value for num sentences is 3 
+    num_sentencess = int(request.args.get("num_sentences", 3))
+    # Summarize the input text
     summary = summarizer(text, num_sentences=num_sentencess)
-    return jsonify({'summary': summary})
+    # Return the summary in JSON format
+    return jsonify({"summary": summary})
 
-@app.route('/summariess', methods=['POST'])
+# Define endpoint to add input text to the list of summaries
+@app.route("/summariess", methods=["POST"])
 def summariess():
-    global counter
+    # Get input text
     data = request.get_json()
-    text = data['text']
-    summaries.append('{} - {}'.format(counter, str(text))+ '\r\n')
-    counter += 1
-    # Return all the summaries in the summaries array
-    return jsonify({'summaries': summaries})
+    text = data["text"]
+    # Append the input text to the list of summaries
+    summaries.append("{}".format(str(text)) + "\r\n")
+    # Return all the summaries in the summaries array in JSON format
+    return jsonify({"summaries": summaries})
 
-@app.route('/toggle_note', methods=['POST'])
+# Define endpoint to toggle the noteOpen variable
+@app.route("/toggle_note", methods=["POST"])
 def toggle_note():
     global noteOpen  # use global keyword to modify the global variable
-    noteOpen = not noteOpen  # toggle the value of noteOpen
+    # Toggle the value of noteOpen
+    noteOpen = not noteOpen
+    # Return the updated value of noteOpen in JSON format
     return jsonify(noteOpen)
 
-@app.route('/get_note', methods=['GET'])
+# Define endpoint to get the value of noteOpen
+@app.route("/get_note", methods=["GET"])
 def get_note():
-    # Return all the summaries in the summaries array
+    # Return the value of noteOpen in JSON format
     return jsonify(noteOpen)
 
-@app.route('/get_summaries', methods=['GET'])
+# Define endpoint to get the list of summaries with their corresponding index numbers
+@app.route("/get_summaries", methods=["GET"])
 def get_summaries():
-    # Return all the summaries in the summaries array
-    return jsonify(summaries)
+    summary_list = []
+    # Loop through the list of summaries and add them with their index numbers to summary_list
+    for i, summary in enumerate(summaries, start=1):
+        summary_list.append("{} - {}".format(i, summary))
+    # Return the list of summaries in summary_list in JSON format
+    return jsonify(summary_list)
 
-@app.route('/delete_summary', methods=['POST'])
+
+# Define a route to delete a summary
+@app.route("/delete_summary", methods=["POST"])
 def delete_summary():
     global summaries
-    global counter
-    counter = 1
+    # Get the index of the summary to delete from the request data
     data = request.get_json()
-    index = int(data['index'])
+    index = int(data["index"])
+    # Check if the index is valid, then delete the corresponding summary
     if index >= 0 and index < len(summaries):
         del summaries[index]
-        return jsonify({'message': 'Summary deleted successfully'})
+        return jsonify({"message": "Summary deleted successfully"})
     else:
-        return jsonify({'error': 'Invalid index'})
+        return jsonify({"error": "Invalid index"})
 
-@app.route('/abstract', methods=['POST'])
+# Define a route to generate an abstract summary
+@app.route("/abstract", methods=["POST"])
 def abstract():
     global abstractSum
+    # Get the input text and the min/max length for the summary from the request data
     data = request.get_json()
-    text = data['text']
+    text = data["text"]
     word_count = len(text.split())
-    min_lengthh = int(request.args.get('min_length', 1))  
-    max_lengthh = int(request.args.get('max_length', 10))  
+    min_lengthh = int(request.args.get("min_length", 1))
+    max_lengthh = int(request.args.get("max_length", 10))
+    # Load the T5 tokenizer
+    tokenizer = AutoTokenizer.from_pretrained("t5-base")
+    # Encode the input text using the tokenizer
+    encoded = tokenizer.encode(data["text"], add_special_tokens=True)
+    # Compute the length of the tokenized sequence
+    seq_length = len(encoded)
+    # Adjust the min/max length if they exceed the word count of the input text
     if min_lengthh > word_count:
         min_lengthh = word_count
         max_lengthh = word_count
     if max_lengthh > word_count:
         max_lengthh = word_count
-    summarys = abstractSum(text, min_length=min_lengthh, max_length=max_lengthh)
-    summary = summarys[0]['summary_text']
-    return jsonify({'summary': summary})
+    # Split the text into smaller sequences of length 500 if it exceeds sequence length 500
+    if seq_length > 500:
+        # Split text into sequences of length 500
+        text_sequences = textwrap.wrap(text, width=500)
+        # Initialize a list to store the summaries
+        summariess = []
+        # Loop through each sequence
+        for sequence in text_sequences:
+            # Tokenize the sequence using the T5 tokenizer
+            encoded = tokenizer.encode(sequence, add_special_tokens=True)
+            # Compute the min/max length for the summary
+            max = round((2 + len(encoded)) * (max_lengthh / word_count))
+            min = round((2 + len(encoded)) * (min_lengthh / word_count))
+            # Generate the summary using the T5 summarization pipeline
+            summary = abstractSum(sequence, min_length=min, max_length=max)[0][
+                "summary_text"
+            ]
+            summariess.append(summary)
 
-@app.route('/analysis', methods=['POST'])
+        # Concatenate the summaries
+        summary = " ".join(summariess)
+    else:
+        # Generate a summary for the whole text using the T5 summarization pipeline
+        summary = abstractSum(
+            text,
+            min_length=round(seq_length * (min_lengthh / word_count)),
+            max_length=round(seq_length * (max_lengthh / word_count)),
+        )[0]["summary_text"]
+
+    # Store the summary in the global variable
+    summaries.append(summary)
+    # Return the summary as a JSON response
+    return jsonify({"summary": summary})
+
+# Define a route for the analysis endpoint that accepts a POST request
+@app.route("/analysis", methods=["POST"])
 def analysis():
+    # Extract the text data from the request body
     data = request.get_json()
-    text = data['text']
+    text = data["text"]
+
     # Create a TextBlob object
     blob = TextBlob(text)
+
     # Get the sentiment polarity (-1 to +1)
     sentiment_polarity = blob.sentiment.polarity
-    # Get the emotion
+
+    # Determine the emotion based on the sentiment polarity
     if sentiment_polarity > 0:
         emotion = "Positive"
     elif sentiment_polarity < 0:
         emotion = "Negative"
     else:
         emotion = "Neutral"
+
     # Get the noun phrases (entities)
     entities = [e.replace("`s", "") for e in blob.noun_phrases]
+
     # Get the part-of-speech tags
     pos_tags = blob.pos_tags
 
+    # Create a dictionary with the analysis results
     results = {
         "sentiment_polarity": sentiment_polarity,
         "emotion": emotion,
         "entities": entities,
-        "pos_tags": pos_tags
+        "pos_tags": pos_tags,
     }
-    # Return the results as JSON
+
+    # Return the analysis results as JSON
     return jsonify(results)
 
-if __name__ == '__main__':
+# Start the Flask application if the script is executed directly
+if __name__ == "__main__":
     app.run(debug=True)
